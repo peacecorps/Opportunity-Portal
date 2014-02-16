9bc8bf9610ab8f605a256f66419ebd9863b21d13
paopow <ksiangliulue@gmail.com>
2014-02-16 04:00:24 -0500

    JSONP

--- data/data_api/app.py
+++ data/data_api/app.py
@@ -1,13 +1,42 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template, jsonify, current_app
+from functools import wraps
 import json
 
 app = Flask(__name__)
+ 
+def support_jsonp(f):
+    """Wraps JSONified output for JSONP"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        callback = request.args.get('callback', False)
+        if callback:
+            content = str(callback) + '(' + str(f().data) + ')'
+            return current_app.response_class(content, mimetype='application/json')
+        else:
+            return f(*args, **kwargs)
+    return decorated_function
+ 
 
 @app.route('/test')
 def test():
     return render_template('index.html')
 
+@app.route('/all_jobs')
+@support_jsonp
+def all_jobs():
+    jobs = None
+    with open('data/pc.json') as f:
+        jobs = json.load(f)
+    return jsonify({'data':jobs})
 
+
+# @app.route('/get_jobs')
+# def get_jobs():
+#     all_jobs = None
+#     with open('data/pc.json' as f):
+#         all_jobs = json.load(f)
+#     sifter = lambda
+#     jobs = filter(sifter, all_jobs)
 
 # @app.route('/')
 # def search_job():
@@ -19,29 +48,6 @@ def foo():
     return "hi"
 
 
-@app.route('/all_jobs')
-def all_jobs():
-    jobs = None
-    with open('data/pc.json') as f:
-        jobs = json.load(f)
-    return jsonify({'data':jobs})
-
-
-# @app.route('/get_jobs')
-# def get_jobs():
-#     filters = request.json
-#     locations = filters.get('country', None)    
-#     start_date = filters.get('start_date')
-#     all_jobs = None
-#     with open('data/pc.json') as f:
-#         all_jobs = json.load(f)
-
-#     jobs = []
-#     for j in all_jobs:
-#         if j['location'] in locations:
-#             jobs.append()
-        
-#     return jsonify({'data':jobs})
 
 if __name__ == '__main__':
     app.run(debug=True)

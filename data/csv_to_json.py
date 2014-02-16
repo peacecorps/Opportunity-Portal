07f85649bdb0ba04d59c73f06b4665badfdfe0f1
paopow <ksiangliulue@gmail.com>
2014-02-16 00:47:26 -0500

    Arrhhhh

---
+++ data/csv_to_json.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import json
+
+
+df = pd.read_csv('data/pc.csv')
+jobs = []
+
+for row_idx, row in df.iterrows():
+    job = {}
+    print row_idx
+    if row_idx > 2:
+        break
+    job['current_req_status'] = row['CURRENT REQ STATUS']
+    job['FY'] = row['FY']
+    job['Q'] = row['Q']
+    job['AA'] = row['AA']
+    job['POST'] = row['POST']
+    job['REQ ID'] = row['REQ ID']
+    job['num_vol'] = row['# VOL REQUESTED']
+    job['title'] = row['PROJECT TITLE']
+    job['language'] = row['LANG']
+    job['nominate_deadline'] = row['LAST DATE TO NOMINATE']
+    job['invite_deadline'] = row['INVITATION DEADLINE']
+    job['STAGING'] = row['STAGING']
+    job['live_cond'] = row['Living Conditions [Living_Cond]']
+    job['live_cond_comm'] = row['Living Conditions Comments [Condition_Comments]']
+    job['skills'] = row['Desired Skills [Desired_Skills]']
+    job['proj_desc'] = row['Project Description [Project_Description]']
+
+    jobs.append(job)
+
+
+
+with open('data/pc.json', 'w') as f:
+    json.dump(jobs, f)

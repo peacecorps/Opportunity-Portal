ce4abfd22aca6c452a0da6c68bb50eb37d8ff85f
Mohamed <mhkane@mit.edu>
2014-06-09 18:10:45 -0400

    models finished, database ready to be created

--- backend_prototype_1/opportunity_load/models.py
+++ backend_prototype_1/opportunity_load/models.py
@@ -6,5 +6,9 @@ class Opportunity(models.Model):
     description=models.CharField(max_length=2000) #Corresponds to the description of the project. Column C of the JSR full extract
     position_remaining=models.IntegerField(default=0) #number of positions remaining for a specific opportunity. Column E of the JSR full extract
     region=models.CharField(max_length=200) #Corresponds to the sub-region part of the excel file. Column G of the JSR full extract
-    start_date=models.DateTimeField('start_date') # Column K 
+    start_date=models.DateTimeField('start_date') 
+    apply_date=models.DateTimeField('apply_date')
+    sector=models.CharField(max_length=200)
+    def __unicode__(self):
+    	return (self.title,self.description)
     
